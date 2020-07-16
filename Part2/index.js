a767eb99c46c9679c257dfe51b87699a43330be9
jimmurphyiv <jim.murphyiv@gmail.com>
2020-07-16 11:22:45 -0400

    done

--- Part2/index.js
+++ Part2/index.js
@@ -21,6 +21,12 @@ hearts.style = 'color: red;'
 const spades = document.querySelector('#spades');
 spades.style = 'color: black;'
 
-const switchCard = card.style.color(colorInput.value);
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+    const card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value;
+  }
 
 
